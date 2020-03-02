55c51e2a79a7bade981b6f7cfda4b1ddc925d4ce
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-03-02 10:47:29 +0100

    Added basic code.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void generate_to_file(char* filename, unsigned long long max_number);
+
+int main(){
+	
+}
+
+void generate_to_file(char* filename, unsigned long long max_number){
+
+}

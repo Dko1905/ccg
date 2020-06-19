6caa77fa45663e1f3e2b9eee5d92e55ac0931cfd
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-06-19 15:19:03 +0200

    Now reads from arguments

--- src/main.c
+++ src/main.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdint.h> // For uintmax_t
+#include <inttypes.h> // For strtoumax
+#include <errno.h> // For errno
 
-typedef uintmax_t llu;
+typedef uintmax_t uintm;
 
-void generate_to_stream(FILE* stream, llu max_number);
+#define BUFFERSIZE
 
-int main(){
-	char* filename = "test.csv";
+void generate_to_stream(FILE* stream, uintm max_number);
+
+// Macro for checking and printing error info
+#define ERRBUFFERSIZE 255
+#define ERR_CHECK(exp, message, filename) \
+	if(exp){ \
+		char str[ERRBUFFERSIZE]; \
+		snprintf(str, ERRBUFFERSIZE, message, filename); \
+		perror(str); \
+		return errno; \
+	}
+
+int main(int argc, char* argv[]){
+	if(argc < 3){ // Check for right amount of arguments
+		fprintf(stderr, "Incorrect usage!\n");
+		printf("Usage: ccg <filename> <max number>\n");
+		return 1;
+	}
+	char* endptr; // From cppreference
+	char* filename = argv[1]; // Unsafe
+	uintm maxNumber = strtoumax(argv[2], &endptr, 10);
+	if(maxNumber < 1){
+		fprintf(stderr, "Incorrect/invalid number!\n");
+		printf("Usage: ccg <filename> <max number>\n");
+		return 1;
+	}
 
 	FILE* fptr = fopen(filename, "w");
+	ERR_CHECK(fptr == NULL, "Failed to open %s", filename);
 
-	generate_to_stream(fptr, 1000000);
-	fclose(fptr);
+	generate_to_stream(fptr, maxNumber);
+	ERR_CHECK(fclose(fptr) == 0, "Failed to close %s", filename);
 }
 
-void generate_to_stream(FILE* stream, llu max_number){
-	llu steps;
-	llu n_temp;
-	for(llu n = 1; n < max_number; n++){
+void generate_to_stream(FILE* stream, uintm max_number){
+	uintm steps;
+	uintm n_temp;
+	for(uintm n = 1; n < max_number; n++){
 		n_temp = n; // by value
 		steps = 0;
 		while(n_temp != 1){

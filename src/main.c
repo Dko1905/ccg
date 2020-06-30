52d92b7e666f6b655a7c69bba7613f81b80da8a4
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-06-30 21:37:04 +0200

    Updated travis

--- src/main.c
+++ src/main.c
@@ -1,61 +1,99 @@
 #include <stdio.h>
-#include <stdint.h> // For uintmax_t
-#include <inttypes.h> // For strtoumax
-#include <errno.h> // For errno
+#include <stdlib.h>
 
-typedef uintmax_t uintm;
+int generate_to_stream(FILE* stream, size_t max, size_t buffer_size);
+int convert_to_csv(FILE* input, FILE* output);
 
-#define BUFFERSIZE
+void test1();
+void test2();
 
-void generate_to_stream(FILE* stream, uintm max_number);
+int main(){
+	test1();
+	printf("Generated\n");
+	test2();
 
-// Macro for checking and printing error info
-#define ERRBUFFERSIZE 255
-#define ERR_CHECK(exp, message, filename) \
-	if(exp){ \
-		char str[ERRBUFFERSIZE]; \
-		snprintf(str, ERRBUFFERSIZE, message, filename); \
-		perror(str); \
-		return errno; \
-	}
-
-int main(int argc, char* argv[]){
-	if(argc < 3){ // Check for right amount of arguments
-		fprintf(stderr, "Incorrect usage!\n");
-		printf("Usage: ccg <filename> <max number>\n");
-		return 1;
-	}
-	char* endptr; // From cppreference
-	char* filename = argv[1]; // Unsafe
-	uintm maxNumber = strtoumax(argv[2], &endptr, 10);
-	if(maxNumber < 1){
-		fprintf(stderr, "Incorrect/invalid number!\n");
-		printf("Usage: ccg <filename> <max number>\n");
-		return 1;
-	}
-
-	FILE* fptr = fopen(filename, "w");
-	ERR_CHECK(fptr == NULL, "Failed to open %s", filename);
-
-	generate_to_stream(fptr, maxNumber);
-	ERR_CHECK(fclose(fptr) != 0, "Failed to close %s", filename);
+	return 0;
 }
 
-void generate_to_stream(FILE* stream, uintm max_number){
-	uintm steps;
-	uintm n_temp;
-	for(uintm n = 1; n < max_number; n++){
-		n_temp = n; // by value
-		steps = 0;
-		while(n_temp != 1){
-			if( (n_temp & 1) == 0){// If even
-				n_temp >>= 1;
-			}
-			else{
-				n_temp = n_temp * 3 + 1;
-			}
-			steps++;
-		}
-		fprintf(stream, "%llu,%llu\n", (unsigned long long)n, (unsigned long long)steps);
+void test1(){
+	char* filename = "input.bin";
+
+	FILE* file = fopen(filename, "w");
+	if(file == NULL){
+		perror("Failed to open file");
+		return;
 	}
+	int r = generate_to_stream(file, 1000000000, BUFSIZ*100);
+	if(r < 0){
+		fprintf(stderr, "Failed to generate to stream\n");
+	}
+
+	fclose(file);
+}
+
+void test2(){
+	char* input = "input.bin";
+	char* output = "output.csv";
+
+	FILE* inputFile = fopen(input, "r");
+	if(inputFile == NULL){
+		perror("Failed to open input file");
+		return;
+	}
+	FILE* outputFile = fopen(output, "w");
+	if(outputFile == NULL){
+		perror("Failed to open input file");
+		return;
+	}
+	convert_to_csv(inputFile, outputFile);
+
+	fclose(inputFile);
+	fclose(outputFile);
+}
+
+int convert_to_csv(FILE* input, FILE* output){
+	size_t max, r;
+	fread(&max, sizeof(size_t), 1, input);
+
+	for(size_t n = max; n > 0 && fread(&r, sizeof(size_t), 1, input) > 0; --n){
+		fprintf(output, "%llu,%llu\n", (unsigned long long)n, (unsigned long long)r);
+	}
+	return 0;
+}
+
+int generate_to_stream(FILE* stream, size_t max, size_t buffer_size){
+	size_t* buffer = malloc(buffer_size*sizeof(size_t));
+	size_t n, s, i, fwr;// n = the current number, s is the amount of numbers, i is the buffer index and fwr is the write result.
+
+	fwrite(&max, sizeof(size_t), 1, stream);
+	i = 0;
+	while((n = max--) > 0){
+		s = 0;
+		while(n != 1){
+			if( (n & 1) == 0){ // Even
+				n >>= 1;
+			}
+			else{ // Odd
+				n = n * 3 + 1;
+			}
+			++s;
+		}
+		buffer[i++] = s + 1;
+		if(i >= buffer_size){
+			i = 0;
+			fwr = fwrite(buffer, sizeof(size_t), buffer_size, stream);
+			if(fwr != buffer_size){
+				return -1;
+			}
+		}
+	}
+	if(i > 0){
+		fwr = fwrite(buffer, sizeof(size_t), i, stream);
+		if(fwr != i){
+			return -1;
+		}
+	}
+
+	free(buffer);
+	return 0;
 }
